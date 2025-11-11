e393564a8d72af91456700e29b52f01c6d09825b
Cédric Belin <cedx@outlook.com>
2025-11-11 22:23:39 +0100

    Revert a previous commit

--- Debug.ps1
+++ Debug.ps1
@@ -5,7 +5,3 @@ Set-StrictMode -Version Latest
 
 Import-Module "$PSScriptRoot/Sql.psd1"
 # Insert the command to be debugged here.
-
-$version = "10.11.7"
-if ($version -notmatch "^(\d+(\.\d+)*)[^\.\d]*") { throw $invalidOperation }
-[version] $Matches.1

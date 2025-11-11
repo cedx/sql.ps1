1b73ab70db365a4abedc8adb9bfb6b67de57dd01
Cédric Belin <cedx@outlook.com>
2025-11-11 21:56:38 +0100

    Fix the `Debug` script [skip ci]

--- Debug.ps1
+++ Debug.ps1
@@ -3,5 +3,9 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version Latest
 
-Import-Module "$PSScriptRoot/Cli.psd1"
+Import-Module "$PSScriptRoot/Sql.psd1"
 # Insert the command to be debugged here.
+
+$version = "10.11.7"
+if ($version -notmatch "^(\d+(\.\d+)*)[^\.\d]*") { throw $invalidOperation }
+[version] $Matches.1

e90b02ab4638cb6026fd2c319d9f03b802ffe82a
Cédric Belin <cedx@outlook.com>
2025-11-29 23:52:09 +0100

    Port the `Invoke-Reader` cmdlet

---
+++ src/Invoke-Reader.cs
@@ -0,0 +1,57 @@
+namespace Belin.Sql.Cmdlets;
+
+using Belin.Sql.Mapping;
+using System.Collections;
+using System.Data;
+
+/// <summary>
+/// Closes the specified database connection.
+/// </summary>
+[Cmdlet(VerbsLifecycle.Invoke, "Reader")]
+[OutputType(typeof(DataAdapter))]
+public class InvokeReader: Cmdlet {
+	
+	/// <summary>
+	/// The SQL query to be executed.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 1)]
+	public required string Command { get; set; }
+
+	/// <summary>
+	/// The connection to the data source.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0)]
+	public required IDbConnection Connection { get; set; }
+		
+	/// <summary>
+	/// The named parameters of the SQL query.
+	/// </summary>
+	[Parameter(Position = 2)]
+	public Hashtable? Parameters { get; set; }
+		
+	/// <summary>
+	/// The positional parameters of the SQL query.
+	/// </summary>
+	[Parameter]
+	public object[]? PositionalParameters { get; set; }
+
+	/// <summary>
+	/// The wait time, in seconds, before terminating the attempt to execute the command and generating an error.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int Timeout { get; set; } = 30;
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() {
+		if (Connection.State == ConnectionState.Closed) Connection.Open();
+
+		using var command =
+			new NewCommand { Command = Command, Connection = Connection, Parameters = Parameters, PositionalParameters = PositionalParameters, Timeout = Timeout }
+			.Invoke<IDbCommand>()
+			.First();
+
+		WriteObject(new DataAdapter(Mapper: new(), Reader: command.ExecuteReader()));
+	}
+}

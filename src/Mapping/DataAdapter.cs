bf5e288d0030743bc90c8c2223fd410c8822269d
Cédric Belin <cedx@outlook.com>
2025-11-29 22:23:39 +0100

    Add the `DataAdapter` record

---
+++ src/Mapping/DataAdapter.cs
@@ -0,0 +1,10 @@
+namespace Belin.Sql.Mapping;
+
+using System.Data;
+
+/// <summary>
+/// Encapsulates a data reader and a data mapper into a single type.
+/// </summary>
+/// <param name="Mapper">The data mapper that can be used to convert the records returned by the reader.</param>
+/// <param name="Reader">The data reader that can be used to iterate over the results of the SQL query.</param>
+public sealed record DataAdapter(DataMapper Mapper, IDataReader Reader);

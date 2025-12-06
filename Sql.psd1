1e66394fcaf5d579e7f7ad714ed27da06752f32c
Cédric Belin <cedx@outlook.com>
2025-12-06 19:37:57 +0100

    Bump the version number

--- Sql.psd1
+++ Sql.psd1
@@ -1,6 +1,6 @@
 @{
 	DefaultCommandPrefix = "Sql"
-	ModuleVersion = "0.7.0"
+	ModuleVersion = "0.8.0"
 	PowerShellVersion = "7.5"
 	RootModule = "bin/Belin.Sql.Cmdlets.dll"
 
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 "Updating the version number in the sources..."
 $version = (Import-PowerShellDataFile "Sql.psd1").ModuleVersion
-foreach ($item in Get-Item "*/*.csproj") {
+foreach ($item in Get-ChildItem "*/*.csproj" -Recurse) {
 	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
 }

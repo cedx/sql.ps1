d45210a6dcb8b5e30926417ea777f078db037ee2
Cédric Belin <cedx@outlook.com>
2025-12-07 12:44:45 +0100

    Add the `Transaction` and `Type` properties

--- src/Sql/QueryOptions.cs
+++ src/Sql/QueryOptions.cs
@@ -1,9 +1,15 @@
 namespace Belin.Sql;
 
+using System.Data;
+
 /// <summary>
 /// Defines the options of a SQL command.
 /// </summary>
 /// <param name="Timeout">The wait time, in seconds, before terminating the attempt to execute the command and generating an error.</param>
+/// <param name="Transaction">The transaction within which the command executes.</param>
+/// <param name="Type">Value indicating how the command is interpreted.</param>
 public sealed record QueryOptions(
-	int Timeout = 30
+	int Timeout = 30,
+	IDbTransaction? Transaction = null,
+	CommandType Type = CommandType.Text
 );

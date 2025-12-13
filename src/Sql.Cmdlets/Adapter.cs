cfb7cc847ce01f449652ffd39f8d17273da478ea
Cédric Belin <cedx@outlook.com>
2025-12-13 19:33:45 +0100

    `Adapter` class implements `IDisposable`

--- src/Sql.Cmdlets/Adapter.cs
+++ src/Sql.Cmdlets/Adapter.cs
@@ -7,7 +7,13 @@ using System.Data;
 /// </summary>
 /// <param name="Mapper">The data mapper that can be used to convert the records returned by the reader.</param>
 /// <param name="Reader">The data reader that can be used to iterate over the results of the SQL query.</param>
-public sealed record Adapter(
-	Mapper Mapper,
-	IDataReader Reader
-);
+public sealed record Adapter(Mapper Mapper, IDataReader Reader): IDisposable {
+
+	/// <summary>
+	/// Releases any resources associated with this object.
+	/// </summary>
+	public void Dispose() {
+		Reader.Close();
+		GC.SuppressFinalize(this);
+	}
+}
